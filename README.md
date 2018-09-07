0f5557ba8574161a917d08ecc39c70a74861b05a
Snervan <defevera@gmail.com>
2018-09-07 19:23:05 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,61 @@
+# Hangman
+
+This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app)
+
+## About
+
+Hangman game created with ReactJS to learn how to use it. 
+Following this exercise : (https://openclassrooms.com/fr/courses/4664381-realisez-une-application-web-avec-react-js/exercises/2119)
+
+## How to compile your code and see what you've done ?
+
+First : You need Node.js installed with a globally access to the "npm packager" (added automatically with Windows Setup Installer version).
+
+Second: Tape `npm install` then once finished tape `npm start` (without quote) in Windows Command Prompt or Linux Console to run the developement server in the project directory
+
+Three : Once used `npm start`, this will launch automatically in a new tab in your default browser
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.<br>
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.<br>
+You will also see any lint errors in the console.
+
+### `npm test`
+
+Launches the test runner in the interactive watch mode.<br>
+See the section about [running tests](#running-tests) for more information.
+
+### `npm run build`
+
+Builds the app for production to the `build` folder.<br>
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+The build is minified and the filenames include the hashes.<br>
+Your app is ready to be deployed!
+
+See the section about [deployment](#deployment) for more information.
+
+### `npm run eject`
+
+**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
+
+If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+
+Instead, it will copy all the configuration files and the transitive dependencies (Webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
+
+You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
+
+## Supported Browsers
+
+Note: Project tested with the lastest version of Google Chrome
+
+By default, the generated project uses the latest version of React.
+
+You can refer [to the React documentation](https://reactjs.org/docs/react-dom.html#browser-support) for more information about supported browsers.
---
+++ src/App.js
@@ -0,0 +1,201 @@
+//App Created by Snervan; Date : September 4th, 2018
+//Last Update : September 7th, 2018
+import React, { Component } from 'react'
+import Keyboard from './Keyboard'
+import GuessWord from './GuessWord'
+import MonPendu from './Pendu'
+import './App.css';
+
+const ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+//Pre-defined dictionnary of words to guess in an array (French words)
+const dictionary = ["Ane", "Mot", "Pseudo", "Snervan", "Dire", "Bateau", "Voiture", "Decision", "Palmure", "Armature", "Velleitaire"]
+/* Pas touche ! Variable constante ci-dessous permettant de savoir le nombre 
+   de fausses réponses max avant d'avoir le pendu complet et activer la condition de défaite (nbre = 11) */
+const ANSWER_MAX = 11 
+
+class App extends Component {
+  state = {
+    keyboard: this.createKeyboard(),
+    indexMatched: [],
+    dico: this.createWord(),
+    guesses: 0,
+    wrongAnswer: 0, //Incrémenté de 1 à chaque mauvaise lettre ou lettre réutilisée
+    lost: false,
+    keyPressed: "",
+    won: false,
+    score: 0,
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if(!this.state.won && !this.state.lost) this.countLettersFound()
+  }
+
+  componentWillMount() {
+    document.addEventListener("keydown", this.handleKeyDown.bind(this))
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleKeyDown.bind(this))
+  }
+
+  createWord() {
+    let random = Math.floor(Math.random() * dictionary.length)
+    var word = dictionary[random]
+    var letters = []
+
+    for (var i = 0; i < word.length; i++) {
+      letters.push(word[i]);
+    }
+
+    return letters
+  }
+
+  handleLetter = letter => {
+    if(!this.state.won && !this.state.lost) {
+      this.setState({prevKey: this.state.keyPressed, keyPressed: letter.toUpperCase()})
+      this.isMatchingDico(letter)
+    }
+  }
+
+  handleKeyDown = event => {
+    let key = event.key.toUpperCase()
+
+    if(!this.state.won && !this.state.lost && (key >= 'A' && key <= 'Z') ) {
+      this.setState({prevKey: this.state.keyPressed, keyPressed: key.toUpperCase()})
+      this.isMatchingDico(key)
+    }
+  }
+
+  isMatchingDico(letter) {
+    const {dico, indexMatched, guesses, wrongAnswer, score} = this.state
+    const [minLetter, majLetter] = [letter.toLowerCase(), letter.toUpperCase()]
+    const newLetter = [minLetter, majLetter]
+
+    if(indexMatched.includes(minLetter) || indexMatched.includes(majLetter)) {
+        this.setState({score: score - 2, wrongAnswer : wrongAnswer + 1})
+    }
+    else if(!dico.includes(majLetter) && !dico.includes(minLetter)) {
+      this.setState({score: score - 1, wrongAnswer : wrongAnswer + 1})
+      this.setState({indexMatched: [...indexMatched, ...newLetter] })
+    } 
+    else {
+      this.setState({score: score + 2})
+      this.setState({indexMatched: [...indexMatched, ...newLetter] })
+    } 
+
+    this.setState({guesses: guesses + 1})
+  }
+
+  countLettersFound() {
+    const {dico, indexMatched, won, lost, wrongAnswer} = this.state
+    let lettersFound = 0
+    
+    for (var i = 0; i < dico.length; i++) {
+      for (var y = 0; y < indexMatched.length; y++) {
+        if(dico[i] === indexMatched[y]) lettersFound++
+      }       
+    }
+
+    if(wrongAnswer >= ANSWER_MAX && !won) {
+        this.setState({lost: true})
+    } else if(lettersFound === dico.length && !lost) {
+        this.setState({won: true})
+    }
+  }
+
+  computeDisplay(letter) {
+    if(this.state.lost) return "visible"
+    return this.state.indexMatched.includes(letter) ? "visible" : "hidden"
+  }
+
+  createKeyboard() {
+    const result = []
+    let i = 0
+
+    while (result.length < ALPHABET.length) {
+      const letter = ALPHABET[i]
+      result.push(letter)
+      i++
+    }
+    return result
+  }
+
+  getKeyboardFeedback(key) {
+    if(key === this.state.keyPressed)
+      return "pressed" 
+    else if(this.state.indexMatched.includes(key)) 
+      return "used"
+    else
+      return ""    
+  }
+
+
+  tryAgain() {
+    this.setState({
+      dico: this.createWord(),
+      keyPressed: "",
+      guesses: 0,
+      indexMatched: [],
+      wrongAnswer: 0,
+      lost: false,
+      won: false,
+      score: 0,
+    })
+  }
+  
+  render() {
+    const {keyboard, dico, wrongAnswer, guesses, score, won, lost} = this.state
+
+    return (
+      <div className="container">
+       <u><h1>Jeu du Pendu</h1></u>
+        {lost && <p style={{fontSize: "14.5px"}}>La réponse était :</p>}
+        <div id="dico">
+          {dico.map((letter, index) => (
+          <GuessWord key={index} index={index} feedback={this.computeDisplay(letter)} letter={letter} />
+          ))}
+        </div>
+
+        { 
+          (!won && !lost)
+          && 
+          <div className="keyboard">
+            {keyboard.map((letter, index) => (
+              <Keyboard letter={letter} key={index} index={index} feedback={this.getKeyboardFeedback(letter)} onMouseDown={this.handleLetter} />
+            ))}
+          </div>
+        }
+
+        <hr />
+
+        {(!lost) && <p>Score : {score}</p>}
+
+        {
+          won
+          &&
+          <div>
+              <p>Vous avez gagné au bout de {guesses} essais. Réessayez ?</p>
+              <button style={{marginTop: "16px", marginBottom: "10px" }} onClick={() => this.tryAgain()} autoFocus>Recommencer</button>
+          </div>
+        }
+
+        {
+          lost
+          &&
+          <div className="whenWon">
+            <p>Vous avez perdu ! Réessayer ?</p>
+            <button style={{ textAlign: "center", marginTop: "11px", marginBottom: "8px"  }} onClick={() => this.tryAgain()} autoFocus>Recommencer</button>
+          </div>
+        }
+
+        {(!won && !lost) && <p>Essais: {guesses}</p>}
+
+        <div className="dessin">
+          <MonPendu wrongAnswer={wrongAnswer} />
+        </div>
+    </div> 
+    );
+  }
+}
+
+export default App;
